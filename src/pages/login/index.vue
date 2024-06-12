7a3c4c63a5b698198094e3e410d332d330d67395
Roberto Ayala <rbt.ayala20@gmail.com>
2024-06-12 16:53:04 -0600

    feat: login has been added

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,27 +1,35 @@
 <template>
-  <section className="min-h-dvh">
-    <div className="w-full flex">
+  <section class="min-h-dvh">
+    <div class="w-full flex">
       <div
-        className="flex basis-full flex-col items-center justify-center gap-4 border-none outline-[5rem] outline-black outline-solid min-h-dvh md:(min-h-inherit basis-50% border-rd-r-[5%])"
+        class="flex basis-full flex-col items-center justify-center gap-4 border-none outline-[5rem] outline-black outline-solid min-h-dvh md:min-h-inherit md:basis-50% login-container"
       >
-        <h1 className="text-5xl font-bold">{t('loginPage.title')}</h1>
-        <p className="text-lg fw-400 op-30">{t('loginPage.description')}</p>
+        <h1 class="text-5xl font-bold">{t('loginPage.title')}</h1>
+        <p class="text-lg fw-400 op-30">{t('loginPage.description')}</p>
+        <FormBox
+          requireSubmit
+          defaultValues="{defaultValues}"
+          formField="{formField}"
+          formSchema="{formSchema}"
+          onSubmit="{handleSubmit}"
+        />
       </div>
       <div
-        className="hidden bg-black text-white min-h-dvh md:(flex basis-50% flex-col)"
+        class="hidden bg-black text-white min-h-dvh md:flex md:basis-50% md:flex-col"
       >
-        <div className="flex items-center justify-end gap-3 p-8">
-          <p className="text-xl font-bold">{t('title')}</p>
+        <div class="flex items-center justify-end gap-3 p-8">
+          <p class="text-xl font-bold">{t('title')}</p>
           <IconResolver
-            icon-class="w-3rem h-3rem"
-            icon-name="i-tdesign:logo-qq"
+            iconClass="w-3rem h-3rem"
+            iconName="i-tdesign:logo-qq"
           />
         </div>
-        <div className="mt-auto">
-          <p className="p-5 text-lg font-bold op-50">
-            {{
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, delectus sint vitae veritatis fuga, natus tenetur mollitia possimus omnis enim laudantium distinctio reiciendis necessitatibus minus soluta at, inventore quibusdam qui.'
-            }}
+        <div class="mt-auto">
+          <p class="p-5 text-lg font-bold op-50" style="opacity: 50%">
+            { 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+            delectus sint vitae veritatis fuga, natus tenetur mollitia possimus
+            omnis enim laudantium distinctio reiciendis necessitatibus minus
+            soluta at, inventore quibusdam qui.' }
           </p>
         </div>
       </div>
@@ -34,6 +42,16 @@ export default {
   name: 'Login'
 }
 </script>
+
+<style lang="css">
+.login-container {
+  outline-style: solid;
+  outline-color: #000;
+  outline-width: 5rem;
+  border-top-right-radius: 5%;
+  border-bottom-right-radius: 5%;
+}
+</style>
 
 <route lang="yaml">
 meta:
